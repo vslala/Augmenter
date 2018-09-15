3f70f5b87f2faafdb005aebff7aa96790d9a3113
varunshr <varunshrivastava@globalpay.com>
2018-09-15 18:25:23 +0530

    first commit

---
+++ src/main/java/com/augmentor/app/AugmentorApp.java
@@ -0,0 +1,11 @@
+package com.augmentor.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AugmentorApp {
+	public static void main(String [] args) {
+		SpringApplication.run(AugmentorApp.class, args);
+	}
+}
